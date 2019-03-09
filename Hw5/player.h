8490212a2d072b449c46563c697cacc3dfda4bcd
John Cormican <jcormica@tcd.ie>
2019-03-09 11:29:19 +0000

    functions for human and machine players

---
+++ Hw5/player.h
@@ -0,0 +1,48 @@
+#include "Board.h"
+#include <vector>
+#include <list>
+
+class player
+{
+public:
+  //bool p1;
+  //std::vector<bool> move_generator(Board board);
+
+  virtual void move_choice(Board *board)=0;
+  //virtual int search(Board board);
+
+  //virtual int evaluation_function();
+};
+
+
+class human_player : public player
+{
+public:
+  bool p1;
+  human_player(bool red);
+  virtual void move_choice(Board *board);
+
+};
+
+class machine_player : public player
+{
+public:
+  bool p1;
+  Board virtual_board;
+  std::list<int> line;
+
+  machine_player(bool red, int n, int m);
+
+  virtual void move_choice(Board *board);
+
+  bool** my_discs(bool p);
+
+  float search(int alpha, int beta, int depth, bool re);
+
+  void MakeFakeMove(bool re, int move, std::list<int> line);
+  void UnmakeFakeMove(bool re, int move, std::list<int> line);
+
+
+  std::vector<bool> moves_generator(Board virtual_board);
+
+};
