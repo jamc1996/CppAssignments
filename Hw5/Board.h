8c8fe052a94dac5f4e90561244e3e18c76367622
John Cormican <jcormica@tcd.ie>
2019-03-09 11:26:50 +0000

    added last move tracking.

--- Hw5/Board.h
+++ Hw5/Board.h
@@ -1,22 +1,35 @@
+#ifndef BOARD_H
+#define BOARD_H
+
 class Board
 {
-private:
+public:
   int nrows;
   int ncols;
   bool* red_spots;
   bool* yellow_spots;
-  int* valids;
   bool** red_entry;
   bool** yellow_entry;
   bool red_win, yellow_win;
+  int* valids;
+  int last_move;
 
 
   // Constructor:
-public:
+
+
   Board(const int n, const int m);
 
   void make_move(bool p, int i);
+  void unmake_move(bool p, int i);
+  void set_lastmove(int move);
+
+  bool check_win(bool** reds);
 
   void print();
 
+//  ~Board();
+
 };
+
+#endif
