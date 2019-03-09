b2ed8a121f43e7ab4614ba6bbdeb85ead697d08c
John Cormican <jcormica@tcd.ie>
2019-03-09 11:29:58 +0000

    allow choice between human and machine player.

--- Hw5/main.cc
+++ Hw5/main.cc
@@ -1,34 +1,88 @@
 #include <iostream>
 #include <unistd.h>
 
-//#include "player.h"
+#include "player.h"
 #include "Board.h"
 
 
 int main(int argc, char *argv[])
 {
-  int c;
-  char red_player = 0; char yellow_player = 0;
+  int c,i;
+  bool red_player = false;
+  bool yellow_player = false;
   while ((c = getopt( argc, argv, "ry")) != -1)
   {
     switch(c)
     {
     case 'r':
-      red_player = 1;
+      red_player = true;
+      break;
     case 'y':
-      yellow_player = 1;
+      yellow_player = true;
+      break;
     case '?':
       break;
     }
   }
+  int n = 6;
+  int m = 7;
+  Board board(n,m);
+  player* r = NULL;
+  player* y = NULL;
 
-  Board board(6,7);
-  board.make_move(true,  3);
-  board.make_move(false,  3);
-  board.make_move(true,  1);
+  if (!red_player)
+  {
+    r = new human_player(true);
+  }
+  else
+  {
+    r = new machine_player(true, n, m);
+  }
 
+  if (!yellow_player)
+  {
+    y = new human_player(false);
+  }
+  else
+  {
+    y = new machine_player(false, n, m);
+
+  }
+
+  for ( i = (n*m)/2; i--;)
+  {
+    r->move_choice(&board);
+    std::cout << "\033[31m" << "Red plays " << board.last_move << "\033[0m" <<std::endl;
+    board.red_win = board.check_win(board.red_entry);
+    if (board.red_win)
+    {
+      break;
+    }
+    y->move_choice(&board);
+    std::cout << "\033[33m" << "Yellow plays " << board.last_move << "\033[0m" << std::endl;
+    board.yellow_win = board.check_win(board.yellow_entry);
+    if (board.yellow_win)
+    {
+      break;
+    }
+  }
   board.print();
 
-  std::cout << sizeof(bool) << std::endl;
+  if (board.red_win) {
+    std::cout << "\033[31m" << " RED PLAYER WINS!!! " << "\033[0m" << std::endl;
+  }
+  else if (board.yellow_win)
+  {
+    std::cout << "\033[33m" << " YELLOW PLAYER WINS!!! " << "\033[0m" << std::endl;
+  }
+  else
+  {
+    std::cout << "DRAW!!!" << std::endl;
+  }
+  // board.make_move(true,  3);
+  // board.make_move(false,  3);
+  // board.make_move(true,  1);
+
+
   return 0;
 }
